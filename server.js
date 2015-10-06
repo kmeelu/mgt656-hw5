79ec3e9de1983b79747f1c9721d8f662f1b4e877
K. Meelu <kmeelu.52@gmail.com>
2015-10-06 23:37:29 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/nickname', function(request, response){
+    response.send('determined-butterfly');
+});
+
 app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
